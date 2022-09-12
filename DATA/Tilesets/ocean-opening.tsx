1241ec6424491aa23706d9ed47dd9be7660fdf65
Luna Software and Art <pierce.workman360@gmail.com>
2022-09-12 09:57:23 -0400

    Change to ZIP format

--- DATA/Tilesets/ocean-opening.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.1" name="ocean-opening" tilewidth="16" tileheight="16" tilecount="300" columns="20">
- <image source="ocean-opening.png" width="320" height="240"/>
-</tileset>
--- DATA/Tilesets/robs-room-tileset.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.1" name="robs-room-tileset" tilewidth="16" tileheight="16" tilecount="300" columns="20">
- <image source="robs-room-tileset.png" width="320" height="240"/>
-</tileset>
